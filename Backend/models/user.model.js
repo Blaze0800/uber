18538d5f7aea46ff5d4599c8853b54c84f0809a8
Aman <amandhiman130@gmail.com>
2025-04-09 11:29:00 +0530

    upating user models and controller

---
+++ Backend/models/user.model.js
@@ -0,0 +1,57 @@
+const e = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+      firstname: {
+        type: String,
+        required: true,
+        minlength: [3, 'First name must be at least 3 characters long'],
+      },
+      lastname: {
+        type: String,
+      
+        minlength: [3, 'Last name must be at least 3 characters long'],
+      }
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select : false,
+    },
+    socketId: {
+      type: String,
+    },
+    
+  })
+
+
+userSchema.methods.generateAuthToken = async function () {
+  const token = jwt.sign({ _id: this._id}, process.env.JWT_SECRET);
+  return token;
+}
+userSchema.methods.comparePassword = async function (password) {
+  const user = this;
+  const isMatch = await bcrypt.compare(password, user.password);
+  return isMatch;
+}
+
+userSchema.methods.hashPassword = async function (password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+
+
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
+
+
